e4fbb26a2a144da401572be8cbd9e00441ed2ccc
Aravind <Aravind@DESKTOP-TC74QKT>
2022-12-16 00:32:38 +0530

    Added Base class

--- Framework/src/test/java/testComponenets/Base.java
+++ Framework/src/test/java/testComponenets/Base.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+public class Base {
     
-  @Test
-  public void shouldAnswerWithTrue() {
-    assertTrue(true);
-  }
+  
 }
